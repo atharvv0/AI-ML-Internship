49097627a59192f0b1ffb51718184e7426f51c3b
Atharva Kshirsagar <atharva0604@hotmail.com>
2026-01-19 14:57:39 +0530

    Add files via upload
    AI-Ml Internship Task-1 complete execution with Report.

---
+++ iris_model.py
@@ -0,0 +1,27 @@
+print("Script started...")
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Load dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Evaluation
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
